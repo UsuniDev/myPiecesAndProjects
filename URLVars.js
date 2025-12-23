0192af51bc2e4d87404b6cbd08a5de206b8e4723
Usuni <hernandezroberto1106.com@gmail.com>
2025-12-22 21:09:04 -0500

    Add files via upload

---
+++ URLVars.js
@@ -0,0 +1,22 @@
+const pokemon = "https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg";
+const fnaf    = "https://upload.wikimedia.org/wikipedia/commons/a/a1/Five_Nights_at_Freddy%27s_2_Logo.png";
+const zelda   = "https://upload.wikimedia.org/wikipedia/commons/2/2a/Zelda_Logo.svg";
+
+
+
+//DRIVE URLS
+
+const pokeDrive  = "https://drive.google.com/drive/folders/1gby5BXLm-kPubS3uAHNf0hl_aa8T8VPW";
+const fnafDrive  = "https://drive.google.com/drive/folders/1ZZ-jEN6UO37a9a3IiWpus9NxOJS6GVCP";
+const zeldaDrive = "https://drive.google.com/drive/folders/184-A7wbxUTZtRk5EqOCMEeITVxKxOuic";
+
+
+/**
+ * https://drive.google.com/drive/folders/1ZZ-jEN6UO37a9a3IiWpus9NxOJS6GVCP
+ *
+ * https://drive.google.com/drive/folders/184-A7wbxUTZtRk5EqOCMEeITVxKxOuic
+ * 
+ * 
+ * 
+ * 
+ */
