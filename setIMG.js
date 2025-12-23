8d774ed7f58a56f6b5a9b6159c82a48d1bf66203
Usuni <hernandezroberto1106.com@gmail.com>
2025-12-22 20:43:35 -0500

    Add files via upload
    guess who's back

---
+++ setIMG.js
@@ -0,0 +1,14 @@
+//FIRST I WILL SET UP THE IMAGES THEN LIK THEM TO DRIVE------------------------
+document.getElementById("pokemon").src = pokemon;       
+document.getElementById("fnaf").src    = fnaf;
+document.getElementById("zelda").src  = zelda;
+
+
+
+function  changeWin( driveFolder){
+    window.location.href= driveFolder;
+
+    
+
+
+}
